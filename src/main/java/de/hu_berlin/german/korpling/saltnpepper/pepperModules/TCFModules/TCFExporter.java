cc0154f372067c60a7042f6b16d6d984bdb0dabf
martink <martin.klotz@hu-berlin.de>
2014-01-27 13:59:55 +0100

    Renamed classes and files

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TCFModules/TCFExporter.java
+++
@@ -1,5 +0,0 @@
-package de.hu_berlin.german.korpling.saltnpepper.pepperModules.TCFModules;
-
-public class TCFExporter {
-
-}
--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TCFModules/TCFManipulator.java
+++
@@ -1,8 +0,0 @@
-package de.hu_berlin.german.korpling.saltnpepper.pepperModules.TCFModules;
-/*
- * @author
- * 
- */
-public class TCFManipulator {
-
-}
